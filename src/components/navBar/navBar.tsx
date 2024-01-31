441f916c3be8da0d261ab197d352c5c45c26817b
Carolimaa <carolniver12@gmail.com>
2024-01-31 20:15:44 -0300

    Adicionando componentes Home, NavBar e Footer

---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,26 @@
+import Logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    
+    return (
+        <>
+        <div className='w-full bg-[#282424] text-white flex justify-center py-4'>
+            <div className="container flex justify-between text-lg">
+                <div className='text-2xl font-bold uppercase'> 
+                <Link to="/">
+                    <img src={Logo} alt="" width="200" />
+                </Link>
+                </div>
+                <div className='flex gap-4'>
+                    <Link to='/produtos' className='hover:underline'>Produtos</Link>
+                    <Link to='/categorias' className='hover:underline'>Categorias</Link>
+                    <Link to='/cadastrarCategoria' className='hover:underline'>Cadastrar categoria</Link>
+            </div>
+            </div>
+            </div>
+        </>
+    )
+    }
+
+    export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,29 @@
+import homeLogo from '../../assets/home.avif'
+
+function Home() {
+    return (
+        <>
+        <div className="bg-white flex justify-center">
+        <div className='container grid grid-cols-2 text-black'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5xl font-bold'>DrogaGen</h2>
+            <p className='text-xl'>A Drogaria que cuida de você!</p>
+
+            <div className="flex justify-around gap-4">
+            
+                <button className='rounded bg-[#282424]  text-white py-2 px-4'>Ver produtos</button>
+            </div>
+            </div>
+
+            <div className="flex justify-center ">
+            <img src={homeLogo} alt="" className='w-2/3' />
+    
+            </div>
+        </div>
+        </div>
+    
+    </>
+    );
+}
+
+export default Home;
